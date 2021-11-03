39b716b1e488566669a672558a9b7530ffbbc76b
Fábio Antunes <fabio.fao03@gmail.com>
2021-11-03 16:53:45 -0300

    page contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
