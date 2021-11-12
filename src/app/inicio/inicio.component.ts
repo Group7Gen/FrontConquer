635e3337d08617bb1480fbb7f29cd21f33f64e5c
matlencar <malencar292@gmail.com>
2021-11-12 15:21:18 -0300

    Implementação da service produto

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { AuthService } from '../service/auth.service';
+import { CategoriaService } from '../service/categoria.service';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private categoriaService: CategoriaService,
+    private authService: AuthService,
+    private produtoService: ProdutoService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    if(environment.token == ''){
+      this.router.navigate(['entrar'])
+    }
+
   }
 
 }
