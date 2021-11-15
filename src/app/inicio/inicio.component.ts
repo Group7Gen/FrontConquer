417868c601a3bb7f7ab52757610cd5ece0f202a0
Fábio Antunes <fabio.fao03@gmail.com>
2021-11-15 17:37:38 -0300

    alteração na page inicio html e ts metodo publicar

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Categoria } from '../model/Categoria';
+import { Produto } from '../model/Produto';
+import { Usuario } from '../model/User';
 import { AuthService } from '../service/auth.service';
 import { CategoriaService } from '../service/categoria.service';
 import { ProdutoService } from '../service/produto.service';
@@ -12,6 +14,20 @@ import { ProdutoService } from '../service/produto.service';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+
+  produto: Produto = new Produto()
+  listaProdutos: Produto[]
+
+  categoria: Categoria = new Categoria()
+  listaCategorias: Categoria[]
+  idCategoria: number
+
+  curso: string
+  descricao: string
+  preco: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
 
 
   constructor(
@@ -26,6 +42,45 @@ export class InicioComponent implements OnInit {
     if(environment.token == ''){
       this.router.navigate(['entrar'])
     }
+    this.getAllCategorias()
+    this.getAllProdutos()
   }
 
+  getAllCategorias() {
+    this.categoriaService.getAllCategoria().subscribe((resp: Categoria[]) => {
+      this.listaCategorias = resp
+    })
+  }
+
+  getAllProdutos() {
+    this.produtoService.getAllProduto().subscribe((resp: Produto[]) => {
+      this.listaProdutos = resp
+    })
+  }
+
+  findByIdCategoria() {
+    this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria) => {
+      this.categoria = resp
+    })
+  }
+
+  publicar() {
+
+    //this.categoria.id = this.idCategoria
+    //this.produto.categoria = this.categoria
+    //this.usuario.id = this.idUsuario
+    //this.produto.usuario = this.usuario
+
+    this.produto.curso = this.curso
+
+    this.produto.descricao = this.descricao
+
+    this.produto.preco = this.preco
+
+    this.produtoService.postProduto(this.produto).subscribe((resp: Produto) => {
+      this.produto = resp
+      alert('Produto cadastrado com sucesso!')
+      this.produto = new Produto()
+    })
+  }
 }
