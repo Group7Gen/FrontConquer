bb823fb7b8a1b92a69999247804b8ebedca9b6d4
matlencar <malencar292@gmail.com>
2021-11-16 00:01:58 -0300

    update produto service

--- src/app/service/produto.service.ts
+++ src/app/service/produto.service.ts
@@ -16,22 +16,22 @@ export class ProdutoService {
   }
 
   getAllProduto(): Observable<Produto[]>{
-    return this.http.get<Produto[]>('http://localhost/produtos', this.token)
+    return this.http.get<Produto[]>('http://localhost:8080/produtos', this.token)
   }
 
   getByIdProduto(id:number): Observable<Produto>{
-    return this.http.post<Produto>(`http://localhost/produtos${id}`, this.token)
+    return this.http.post<Produto>(`http://localhost:8080/produtos${id}`, this.token)
   }
 
   postProduto(produto: Produto): Observable<Produto>{
-    return this.http.post<Produto>('http://localhost/produtos', produto, this.token)
+    return this.http.post<Produto>('http://localhost:8080/produtos', produto, this.token)
   }
 
   putProduto(produto: Produto): Observable<Produto>{
-    return this.http.put<Produto>('http://localhost/produtos', produto, this.token)
+    return this.http.put<Produto>('http://localhost:8080/produtos', produto, this.token)
   }
 
   deleteProduto(id: number){
-    return this.http.delete(`http://localhost/produtos/${id}`, this.token)
+    return this.http.delete(`http://localhost/produtos:8080/${id}`, this.token)
   }
 }
